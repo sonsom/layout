6415873885135c8af93e4e0a80b25176232e48fb
xuanson <sondzuong3293@gmail.com>
2018-03-09 13:34:53 +0700

    layoyt psd

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+
+</head>
+<body>
+<!--Wrapper-->
+    <div class="wrapper">
+        <!--header-->
+        <div class="header">
+            <div class="logo"><img src="images/logo.png" alt=""></div>
+            <div class="menu">
+                   <ul>
+                   <li><a href="" id="home">Home</a></li>
+                   <li><a href="" id="">About Us</a></li>
+                   <li><a href="" id="">Our Service</a></li>
+                   <li><a href="" id="">Prices</a></li>
+                   <li><a href="" id="">Contact</a></li>
+                   </ul>
+            </div>
+            <div id="slogan"><span id="mache">MACHE</span> FREE PSD TEMPLATE</div>
+            <div id="line"><img src="images/line.jpg" alt=""></div>
+            <div class="text1">
+                <div id="text11">Elegant and clean PSD template, which you can download and use for free!</div>
+                <div id="text12">You can easily modify the design to make it your own and help you get your project running faster.</div>
+            </div>
+            <div class="show">Show More</div>
+        </div>
+        <!--end header-->
+        <!--video-->
+        <div class="video">
+            <div class="underhead"></div>
+            <div class="text-video">
+                <p>Powerfull PSD template available for free!</p>
+                <img src="images/line.jpg" alt="">
+                <div id="text-video2">
+                 Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                 aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                 voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                 occaecat cupidatat non proident, sunt in culpa
+                </div>
+            </div>
+            <div class="seevideo">
+                    <p class="see">See The Video</p>
+                    <div class="see1"><img src="images/see1.png" alt=""></div>
+                    <div class="see2"><img src="images/see2.png" alt=""></div>
+            </div>
+        </div>
+        <!--end video-->
+        <!--mac-->
+         <div class="mac">
+            <div id="mac1"><img src="images/mac.png" alt=""></div>
+            <div id="mac2"><img src="images/contentmac..jpg" alt=""></div>
+            <div id="mac3"><img src="images/undermac.png" alt=""></div>
+         </div>
+        <!--end mac-->
+        <!--service-->
+        <div class="service">
+            <div class="textser">
+            <div id="textser1">Our Services</div>
+            <div id="line3"><img src="images/line.jpg" alt=""></div>
+            <div id="textser2">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et <p>dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation</p></div>
+            </div>
+            <div class="icon-service">
+                <div id="ms"><img src="images/s1.jpg" alt=""></div>
+                <div id="ms"><img src="images/s2.jpg" alt=""></div>
+                <div id="ms"><img src="images/s3.jpg" alt=""></div>
+                <div id="ms"><img src="images/s4.jpg" alt=""></div>
+            </div>
+            <div class="text-iconser">
+                <div id="ts">FULLY EDITABLE</div>
+                <div id="ts">FREE DOWNLOAD</div>
+                <div id="ts">MODERN DESIGN</div>
+                <div id="ts">100% RESPONSIVE</div>
+            </div>
+
+        </div>
+        <!--end service-->
+        <!--slide show-->
+            <div class="sideshow">
+                <div class="content-slide">
+                    <div id="next1"><img src="images/next1.png" alt=""></div>
+                    <div id="note1"><img src="images/note1.png" alt=""></div>
+                    <div id="content-note"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, std do eiusmod tempor
+incididunt ut labore tet dolore magna aliqua.  Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex. </p></div>
+                    <div id="note2"><img src="images/note2.png"></div>
+                    <div id="next2"><img src="images/next2.png" alt=""></div>
+
+                </div>
+
+            </div>
+        <!--end sileshow-->
+        <!--Buy-->
+        <div class="buy">
+            <div class="textbuy">
+            <div id="textbuy1">Check Our Prices</div>
+            <div id="line4"><img src="images/line.jpg" alt=""></div>
+            <div id="textbuy2">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+             sed do eiusmod tempor incididunt ut <p>labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation</p></div>
+            </div>
+            <!--boxbuy-->
+            <div class="boxbuy">
+                    <div class="box1">
+                        <div id="topbox1"><p id="price">$19</p><p id="month">Permonth</p></div>
+                        <div id="downbox1"><div id="text-down">5 Projects<br> 200 MB Storage<br> Notification settings<br>
+                            Password protection</div></div>
+                        <div id="stander">STANDAR</div>
+                        <div id="buynow">BUY NOW</div>
+                    </div>
+
+                    <div class="box2">
+                        <div id="topbox1"><p id="price">$19</p><p id="month">Permonth</p></div>
+                        <div id="downbox1"><div id="text-down">15 Projects<br> 1 GB Storage<br> Notification settings<br>
+                            Password protection</div></div>
+                        <div id="stander">BUSINESS</div>
+                        <div id="buynow">BUY NOW</div>
+                    </div>
+
+                    <div class="box3">
+                        <div id="topbox3"><p id="price">$19</p><p id="month">Permonth</p></div>
+                        <div id="downbox1"><div id="text-down">30 Projects<br> 2 GB Storage<br> Notification settings<br>
+                            Password protection</div></div>
+                        <div id="stander">PRIMEUM</div>
+                        <div id="buynow">BUY NOW</div>
+                    </div>
+
+                    <div class="box4">
+                        <div id="topbox4"><p id="price">$19</p><p id="month">Permonth</p></div>
+                        <div id="downbox1"><div id="text-down">Unlimit Project<br> Unlimit Storage<br> Notification settings<br>
+                            Password protection</div></div>
+                        <div id="stander">UNLIMITED</div>
+                        <div id="buynow">BUY NOW</div>
+                    </div>
+
+            </div>
+            <!--end boxbuy-->
+        </div>
+        <!--end Buy-->
+
+        <!--camera-->
+        <div class="camera">
+            <div class="cam1">
+                <div class="cam11">
+                    <img src="images/cam1.png">
+                    <div class="title">
+                    <div id="title-cam">HIGH RESOLUTION</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+                 <div class="cam22">
+                    <img src="images/cam2.png">
+                    <div class="title">
+                    <div id="title-cam">PHOTOGRAPHY</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+                <div class="cam33">
+                    <img src="images/cam3.png">
+                    <div class="title">
+                    <div id="title-cam">EASY TO CUSTOMIZE</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+                <div class="cam44">
+                    <img src="images/cam4.png">
+                    <div class="title">
+                    <div id="title-cam">24/7 SUPPORT</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+                 <div class="cam55">
+                    <img src="images/cam5.png">
+                    <div class="title">
+                    <div id="title-cam">POWERFULL THEME</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+                <div class="cam66">
+                    <img src="images/cam6.png">
+                    <div class="title">
+                    <div id="title-cam">SEO OPTIMIZATION</div>
+                    <div id="conten-cam">Lorem ipsum dolor sit amet,
+                    adipisicing elit, sed do eiusmod tempor
+                     incididunt ut labore et dolore magna aliqua. Ut </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--end camera-->
+        <!--becam-->
+            <div class="become">
+                <div id="text-become">BECAME A PART OF <span id="mache">MACHE</span> COMUNITY TODAY</div>
+                <div id="btn-join">Join Us</div>
+            </div>
+        <!--end becam-->
+
+        <!--update-->
+        <div class="update">
+            <div class="text-update">
+                <div id="textup1"><p>Our Latest Updates</p></div>
+                <div id="line5"><img src="images/line.jpg" alt=""></div>
+                <div id="textup2">Lorem ipsum dolor sit amet, consectetur
+                 adipisicing elit, sed do eiusmod tempor incididunt ut labore et<p> dolore magna aliqua.
+                 Ut enim ad minim veniam, quis nostrud exercitation</p></div>
+            </div>
+            <div class="image-update">
+                <div class="imgup">
+                    <div class="imgup11"><img src="images/update1.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+
+                <div class="imgupdate">
+                    <div class="imgup11"><img src="images/updare22.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+
+                <div class="imgupdate2">
+                    <div class="imgup11"><img src="images/update3.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+
+                 <div class="imgupdate3">
+                    <div class="imgup11"><img src="images/updat4.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+
+                <div class="imgupdate4">
+                    <div class="imgup11"><img src="images/update5.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+
+                 <div class="imgupdate5">
+                    <div class="imgup11"><img src="images/update6.png"></div>
+                    <div class="imgup1">
+                        <div id="content-update">LOREM IPSUMA DOLOR SIT</div>
+                        <div id="text-update">Lorem ipsum dolor sit amet, consectetur elit,
+                         sed do eiusmod tempor incididunt ut labore et </div>
+                    </div>
+                </div>
+                <div class="showmore">SHOW MORE</div>
+                <div class="imgshow"><img src="images/iconshow.png" alt=""></div>
+            </div>
+        </div>
+        <!--end update-->
+        <!--free-->
+        <div class="free">
+            <div class="free-1">
+                <div id="slogan1">Mache Free PSD Template</div>
+                <div id="line6"><img src="images/line.jpg" alt=""></div>
+                <div class="textfree">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et<p> dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip<p> ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse<p>
+                    cillum dolore eu fugiat nulla pariatur.Excepteur sint occaecat cupidatat non proident,<p>
+                    sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+                <div id="showmore">Show More</div>
+            </div>
+        </div>
+        <!--end free-->
+        <!--dowload-->
+            <div class="dowload">
+                <div class="decktop">
+                    <div id="topdeck"><img src="images/top.png" alt=""></div>
+                    <div id="decktop"><img src="images/decktop.png" alt=""></div>
+                </div>
+                <div class="text-dowload">
+                    <div id="please">Please share some love by appreciate it and download!</div>
+                    <div id="line7"><img src="images/line.jpg"></div>
+                    <div id="please-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit
+                        esse cillum dolore eu fugiat nulla pariatur.
+                        Excepteur sint occaecat cupidatat non proident,
+                        sunt in culpa
+                    </div>
+                    <div id="btn-dowload">Dowload Now</div>
+                </div>
+            </div>
+        <!--end dowload-->
+        <!--footer-->
+        <div class="footer">
+            <div class="footer1">© 2015 COPYRIGHT MACHE TEMPLATE</div>
+            <div class="footer2">DESIGNED BY</div>
+            <div class="logo-footer"><img src="images/logo-footer.png"></div>
+        </div>
+        <!--end footer-->
+
+    </div>
+<!--end wrapper-->
+</body>
+</html>
